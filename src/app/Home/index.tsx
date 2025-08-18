350d0bab19e2dfd2dfe0ab4213ef9ca2229b32a2
AmandaCMLFerraz <amandaferraz@unochapeco.edu.br>
2025-08-17 22:09:42 -0300

    :sparkles: Componente Button

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,10 +1,12 @@
 import { Text, View } from 'react-native';
 import { styles } from './styles'
+import { Button } from '../../../components/Button';
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Teste</Text>
+      <Text style={styles.text}>Vitrine Dev</Text>
+      <Button title='Buscar' activeOpacity={0.8}/>
     </View>
   );
 }
--- src/app/Home/styles.ts
+++ src/app/Home/styles.ts
@@ -3,12 +3,14 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
+    paddingTop: 62,
+    padding: 24,
+    gap: 24,
   },
   text: {
     fontSize: 32,
     fontWeight: 700,
-    color: "blue",
+    color: "#000",
   }
 })
